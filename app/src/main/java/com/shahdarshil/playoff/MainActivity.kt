4df6b5b411ad8cca4b29f1346968f2450da05a03
Shah Darshil <darshilshah15575@gmail.com>
2021-03-26 11:32:05 +0530

    Initial commit
    Creating Android project with view binding.

---
+++ app/src/main/java/com/shahdarshil/playoff/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shahdarshil.playoff
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
