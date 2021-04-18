c5ad8848bc96dd8d372e309733b595d804c81c73
Shah Darshil <darshilshah15575@gmail.com>
2021-04-18 17:01:53 +0530

    Playoff: Added the network related code by implementing the retrofit2 service and using the REST API Playoff.
    Added the data models required to transform the network calls to the objects using the gson converter factory in retrofit2.

---
+++ app/src/main/java/com/shahdarshil/playoff/network/PlayoffApiService.kt
@@ -0,0 +1,36 @@
+package com.shahdarshil.playoff.network
+
+import com.google.gson.GsonBuilder
+import com.shahdarshil.playoff.network.datamodel.User
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+/**
+ * Created by shah on 14 April, 2021.
+ */
+private const val BASE_URL = "http://192.168.43.173:8080"
+
+interface PlayoffApiService {
+    @POST("/user/login")
+    suspend fun validateUser(@Body user: User): Response<User>
+
+    @POST("/user/register")
+    suspend fun registerUser(@Body user: User): Response<User>
+
+    @POST("/user/authenticate")
+    suspend fun requestOtp(@Body user: User): Response<Any>
+}
+
+private val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+object PlayoffApi {
+    val retrofitService: PlayoffApiService by lazy {
+        retrofit.create(PlayoffApiService::class.java)
+    }
+}
---
+++ app/src/main/java/com/shahdarshil/playoff/network/datamodel/User.kt
@@ -0,0 +1,18 @@
+package com.shahdarshil.playoff.network.datamodel
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+/**
+ * Created by shah on 14 April, 2021.
+ */
+@Parcelize
+data class User(
+    @SerializedName("name")
+    val username: String?,
+    val password: String,
+    val email: String,
+    @SerializedName("oneTimePassword")
+    val otp: String?
+) : Parcelable
