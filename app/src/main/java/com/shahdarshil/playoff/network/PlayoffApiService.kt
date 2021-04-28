0029b1e583feb61fb39e72ab70574c3a4d3e617d
Shah Darshil <darshilshah15575@gmail.com>
2021-04-28 09:14:40 +0530

    Playoff: Added the paths for joinTournament, join Team and others to send the json request to the Playoff API.

--- app/src/main/java/com/shahdarshil/playoff/network/PlayoffApiService.kt
+++ app/src/main/java/com/shahdarshil/playoff/network/PlayoffApiService.kt
@@ -1,15 +1,13 @@
 package com.shahdarshil.playoff.network
 
-import com.google.gson.GsonBuilder
+import com.shahdarshil.playoff.network.datamodel.Player
+import com.shahdarshil.playoff.network.datamodel.Team
 import com.shahdarshil.playoff.network.datamodel.Tournament
 import com.shahdarshil.playoff.network.datamodel.User
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 /**
  * Created by shah on 14 April, 2021.
@@ -28,6 +26,33 @@ interface PlayoffApiService {
 
     @GET("/tournament/{location}")
     suspend fun getTournaments(@Path("location") location: String): Response<List<Tournament>?>
+
+    @GET("/tournament/id/{tournament}")
+    suspend fun getTournament(@Path("tournament") tournament: String): Response<Tournament>
+
+    @POST("/tournament/join")
+    suspend fun joinTournament(@Body team: Team): Response<Team>
+
+    @PUT("/tournament/create")
+    suspend fun createTournament(@Body tournament: Tournament): Response<Tournament>
+
+    @GET("/team/")
+    suspend fun getTeams(): Response<List<Team>?>
+
+    @GET("/team/details/{teamName}")
+    suspend fun getTeam(@Path("teamName") teamName: String): Response<Team>
+
+    @GET("/team/{teamName}/players")
+    suspend fun getTeamPlayers(@Path("teamName") teamName: String): Response<List<User>>
+
+    @POST("/team/join")
+    suspend fun joinTeam(@Body player: Player): Response<Player>
+
+    @POST("/team/create")
+    suspend fun createTeam(@Body team: Team): Response<Team>
+
+    @GET("/team/{player}")
+    suspend fun getPlayerTeam(@Path("player") player: String): Response<Player>
 }
 
 private val retrofit = Retrofit.Builder()
