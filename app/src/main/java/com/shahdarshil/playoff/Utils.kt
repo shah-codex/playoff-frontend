23bb62a885d826e369ace82d3bc6b1e59885c1d2
Shah Darshil <darshilshah15575@gmail.com>
2021-04-28 09:23:05 +0530

    Playoff: Added the extension function toUnixTimestamp() that takes a date as a argument and converts it to unix timestamp. Modified the code for extension function toDate() that takes the unix timestamp as argument and converts it to the representational date.

--- app/src/main/java/com/shahdarshil/playoff/Utils.kt
+++ app/src/main/java/com/shahdarshil/playoff/Utils.kt
@@ -2,7 +2,7 @@ package com.shahdarshil.playoff
 
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
-import java.sql.Date
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -19,9 +19,20 @@ fun String.createHash(): String {
     return digest?.digest(this.toByteArray())?.joinToString("") { "%02x".format(it) } ?: ""
 }
 
-fun Int.toDate(): String {
-    val dateFormatter = SimpleDateFormat("dd-mm-yyyy hh:mm", Locale.US)
-    val date = Date(this.toLong() * 1000)
+fun Long.toDate(): String {
+    val dateFormatter = SimpleDateFormat("d-M-y H:m", Locale.US)
+    val date = Date(this * 1000)
 
     return dateFormatter.format(date)
+}
+
+fun String.toUnixTimestamp(): Long {
+    val formatter = SimpleDateFormat("d-M-y H:m", Locale.US)
+    val date = try {
+        formatter.parse(this)
+    } catch(e: ParseException) {
+        null
+    }
+
+    return date?.time?.div(1000) ?: 0
 }
