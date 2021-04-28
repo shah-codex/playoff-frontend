b1355f0c4ef295087910e4f24d0613728686bbb0
Shah Darshil <darshilshah15575@gmail.com>
2021-04-28 09:24:06 +0530

    Playoff: Added the code for navigation using the live data and click press for the button for detail activity.

--- app/src/main/java/com/shahdarshil/playoff/DetailViewModel.kt
+++ app/src/main/java/com/shahdarshil/playoff/DetailViewModel.kt
@@ -4,11 +4,37 @@ import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.shahdarshil.playoff.network.PlayoffApi
+import com.shahdarshil.playoff.network.datamodel.Player
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.net.ConnectException
 
 /**
  * Created by shah on 18 April, 2021.
  */
 class DetailViewModel() : ViewModel() {
+    private val coroutineJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + coroutineJob)
+
+    private val _teamName = MutableLiveData<String?>()
+
+    val teamName: LiveData<String?>
+        get() = _teamName
+
+    private val _navigateCreateTeam = MutableLiveData<Boolean?>(null)
+
+    val navigateCreateTeam: LiveData<Boolean?>
+        get() = _navigateCreateTeam
+
+    private val _navigateCreateTournament = MutableLiveData<Boolean?>(null)
+
+    val navigateCreateTournament: LiveData<Boolean?>
+        get() = _navigateCreateTournament
+
     private val _createFloatingActionButtonText = MutableLiveData<String>("Create")
 
     val createFloatingActionButtonText: LiveData<String>
@@ -19,11 +45,42 @@ class DetailViewModel() : ViewModel() {
     val createFloatingActionButtonState: LiveData<Boolean>
         get() = _createFloatingActionButtonState
 
+    fun navigateToCreateTournament() {
+        _navigateCreateTournament.value = true
+    }
+
+    fun navigateToCreateTeam() {
+        _navigateCreateTeam.value = true
+    }
+
+    fun onNavigateComplete() {
+        _navigateCreateTournament.value = null
+        _navigateCreateTeam.value = null
+    }
+
     fun changeCreateFloatingActionButtonState() {
         _createFloatingActionButtonState.value = !(_createFloatingActionButtonState.value!!)
         _createFloatingActionButtonText.value = when(_createFloatingActionButtonState.value) {
             true -> "Create"
             else -> "Cancel"
+        }
+    }
+
+    fun getPlayerTeam() {
+        coroutineScope.launch {
+            val response = try {
+                PlayoffApi.retrofitService.getPlayerTeam(DetailActivity.EMAIL)
+            } catch(e: ConnectException) {
+                null
+            }
+
+            viewModelScope.launch {
+                if (response != null && response.isSuccessful) {
+                    _teamName.value = response.body()?.teamName
+                } else {
+                    _teamName.value = null
+                }
+            }
         }
     }
 }
