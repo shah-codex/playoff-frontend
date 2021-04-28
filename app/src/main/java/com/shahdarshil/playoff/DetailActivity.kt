c01f39a60e91f48bb6cf76b45767aa131b8f2df6
Shah Darshil <darshilshah15575@gmail.com>
2021-04-28 09:20:55 +0530

    Playoff: Added code for getting the email address for the use and calling the team of user through ViewModel. Also added the code for onBackPressed() to clear the backstack and exit the application. Added the code to show the dialog when use presses the create team or create tournament.

--- app/src/main/java/com/shahdarshil/playoff/DetailActivity.kt
+++ app/src/main/java/com/shahdarshil/playoff/DetailActivity.kt
@@ -7,22 +7,37 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.navArgs
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import com.shahdarshil.playoff.databinding.ActivityDetailBinding
+import com.shahdarshil.playoff.player.team.CreateTeamDialog
+import com.shahdarshil.playoff.player.team.TeamFragment
+import com.shahdarshil.playoff.player.tournament.CreateTournamentDialog
 import com.shahdarshil.playoff.player.tournament.TournamentFragment
 import com.shahdarshil.playoff.player.update.RecentFragment
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var viewModel: DetailViewModel
+    private val args: DetailActivityArgs by navArgs()
+
+    companion object {
+        lateinit var EMAIL: String
+        var TEAM: String? = null
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding: ActivityDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
 
+        EMAIL = args.email
+
         viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+
+        viewModel.getPlayerTeam()
 
         binding.viewModel = viewModel
 
@@ -31,12 +46,34 @@ class DetailActivity : AppCompatActivity() {
         val fragmentList = arrayListOf<Fragment>(
             RecentFragment(),
             TournamentFragment(),
-            RecentFragment()
+            TeamFragment()
         )
 
         supportActionBar?.also {
             it.elevation = 0.0f
         }
+
+        viewModel.teamName.observe(this, Observer {
+            it?.let {
+                TEAM = it
+            }
+        })
+
+        viewModel.navigateCreateTeam.observe(this, Observer {
+            it?.let {
+                val createTeamDialog = CreateTeamDialog()
+                createTeamDialog.show(supportFragmentManager, "CreateTeamDialog")
+                viewModel.onNavigateComplete()
+            }
+        })
+
+        viewModel.navigateCreateTournament.observe(this, Observer {
+            it?.let {
+                val createTournamentDialog = CreateTournamentDialog()
+                createTournamentDialog.show(supportFragmentManager, "CreateTournamentDialog")
+                viewModel.onNavigateComplete()
+            }
+        })
 
         binding.viewPager.adapter = ViewPagerAdapter(fragmentList, supportFragmentManager, lifecycle)
 
@@ -56,6 +93,10 @@ class DetailActivity : AppCompatActivity() {
                 }
             }
         }.attach()
+    }
+
+    override fun onBackPressed() {
+        finishAffinity()
     }
 }
 
